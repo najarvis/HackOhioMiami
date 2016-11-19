ef028481f210382c2b97bd6bbaeeb1ed04c465a3
NickWayne <wazzup771@gmail.com>
2016-11-19 14:30:26 -0500

    Fixed Collision.js

--- HackOhio/Collisions.js
+++ HackOhio/Collisions.js
@@ -3,33 +3,26 @@
 function clamp(val, min, max) {
     return Math.max(min, Math.min(max, val))
 }
+function circleRectCollision(circle, rectangle){
+    // Find the closest point to the circle within the rectangle
+    // Assumes axis alignment! ie rect must not be rotated
+    var closestX = clamp(circle.X, rectangle.x, rectangle.x + rectangle.width);
+    var closestY = clamp(circle.Y, rectangle.y, rectangle.y + rectangle.height);
 
-// Find the closest point to the circle within the rectangle
-// Assumes axis alignment! ie rect must not be rotated
-var closestX = clamp(circle.X, rectangle.x, rectangle.x + rectangle.width);
-var closestY = clamp(circle.Y, rectangle.y, rectangle.y + rectangle.height);
+    // Calculate the distance between the circle's center and this closest point
+    var distanceX = circle.X - closestX;
+    var distanceY = circle.Y - closestY;
 
-// Calculate the distance between the circle's center and this closest point
-var distanceX = circle.X - closestX;
-var distanceY = circle.Y - closestY;
+    // If the distance is less than the circle's radius, an intersection occurs
+    var distanceSquared = (distanceX * distanceX) + (distanceY * distanceY);
+    return distanceSquared < (circle.Radius * circle.Radius);
+}
 
-// If the distance is less than the circle's radius, an intersection occurs
-var distanceSquared = (distanceX * distanceX) + (distanceY * distanceY);
-return distanceSquared < (circle.Radius * circle.Radius);
-
-// expensive alternative:
-function intersects(circle, rect) {
-    circleDistance.x = Math.abs(circle.x - rect.x);
-    circleDistance.y = Math.abs(circle.y - rect.y);
-
-    if (circleDistance.x > (rect.width/2 + circle.r)) { return false; }
-    if (circleDistance.y > (rect.height/2 + circle.r)) { return false; }
-
-    if (circleDistance.x <= (rect.width/2)) { return true; } 
-    if (circleDistance.y <= (rect.height/2)) { return true; }
-
-    cornerDistanceSq = Math.sqr(circleDistance.x - rect.width/2) +
-                        Math.sqr(circleDistance.y - rect.height/2);
-
-    return (cornerDistanceSq <= (Math.sqr(circle.r)));
+function circleCircleCollision(circle1,circle2){
+    var distance = circle1.pos.distanceTo(circle2)
+    if distance <= (circle1.radius + circle2.radius){
+        return false
+    }else{
+        return true
+    }
 }
