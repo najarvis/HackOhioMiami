6c59741e6cc4041b929329053eb8410a42004c07
JarvisTheJelly <najarvis2016@gmail.com>
2016-11-19 14:31:49 -0500

    Added GameHandler.js

---
+++ HackOhio/GameHandler.js
@@ -0,0 +1,34 @@
+class GameHandler {
+
+    // Initialize the GameHandler. width and height are the width and
+    // height of the canvas.
+    constructor(width, height) {
+    
+        this.width = width;
+        this.height = height;
+        this.entities = [];
+    }
+
+    addEntity(entity) {
+        this.entities.push(entity);
+    }
+
+    // Removes the given entity from the array
+    removeEntity(entity) {
+        var i = this.entities.indexOf(entity);
+        if (i != -1) this.entities = this.entities.splice(i, 1);
+    }
+
+    update() {
+        for (var i = 0; i < this.entities.length; i++) {
+            this.entities[i].update();
+        }
+    }
+
+    draw() {
+        for (var i = 0; i < this.entities.length; i++) {    
+            this.entities[i].draw();
+        }
+    }
+}
+
